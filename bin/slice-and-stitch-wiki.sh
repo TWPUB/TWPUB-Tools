e1d39cd37e922621b692ad751fb0bcd51e033e09
jeremy@jermolene.com <jeremy@jermolene.com>
2022-10-25 17:03:53 +0100

    Update stitching demo to be able to stitch multiple tiddlers into the file

--- bin/slice-and-stitch-wiki.sh
+++ bin/slice-and-stitch-wiki.sh
@@ -2,10 +2,9 @@
 
 # Slice an epub into a twpub plugin and then stitch it into a prebuilt empty wiki file
 
-# ./bin/slice-and-stitch-wiki.sh <path-to-epub-file> <path-to-wiki-file>
+# ./bin/slice-and-stitch-wiki.sh <path-to-epub-file>
 
 EPUB_FILEPATH=$1
-WIKI_FILEPATH=$2
 
 if [ -z "$EPUB_FILEPATH" ]; then
 	echo "Missing parameter: path to epub file"
@@ -17,24 +16,25 @@ if [ ! -f "$EPUB_FILEPATH" ]; then
 	exit 1
 fi
 
-if [ -z "$WIKI_FILEPATH" ]; then
-	echo "Missing parameter: path to wiki file"
-	exit 1
-fi
-
 mkdir -p tmp || exit 1
 
 # Build an almost empty wiki file, just containing the plugins we need
 
-npx tiddlywiki ++plugins/twpub-tools/ ./wikis/immateriel.fr/ \
+npx tiddlywiki ++plugins/twpub-tools/ ++twpubs/using-twpub/ ./wikis/twpub-tools.org/ \
 	--output tmp \
-	--render dummy empty.html text/plain $:/core/templates/tiddlywiki5.html saveTiddlerFilter "$:/core $:/themes/tiddlywiki/snowwhite $:/themes/tiddlywiki/vanilla $:/plugins/immateriel/twpub-tools $:/languages/fr-FR $:/config/Search/AutoFocus $:/config/TiddlerInfo/Mode $:/plugins/tiddlywiki/dynannotate +[sort[title]]" || exit 1
+	--render dummy empty.html text/plain $:/core/templates/tiddlywiki5.html saveTiddlerFilter "$:/config/PageColumns/NewRiver $:/config/PageColumns/Sidebar $:/config/Search/AutoFocus $:/config/stories/main $:/config/TiddlerInfo/Mode $:/core $:/languages/fr-FR $:/plugins/immateriel/twpub-tools $:/plugins/immateriel/using-twpub $:/plugins/tiddlywiki/dynannotate $:/themes/tiddlywiki/snowwhite $:/themes/tiddlywiki/vanilla +[sort[title]]" || exit 1
 
 # Slice the epub
 
 node ./epub2twpub/index.js --epub "$EPUB_FILEPATH" --output ./tmp/twpub.json || exit 1
 
+# Create another couple of JSON files, showing how they can contain arrays or individual tiddlers
+
+echo '{"title":"Build Test","text":"This is a test"}' > ./tmp/tiddler1.json
+
+echo '[{"title":"Build Test 2","text":"This is a test"},{"title":"Build Test 3","text":"This is a test"}]' > ./tmp/tiddler2.json
+
 # Splice the TWPub into the wiki file
 
-node ./bin/stitch-wiki.js ./tmp/twpub.json ./tmp/empty.html ./tmp/index.html || exit 1
+node ./bin/stitch-wiki.js ./tmp/empty.html ./tmp/index.html ./tmp/twpub.json ./tmp/tiddler1.json ./tmp/tiddler2.json || exit 1
 
--- bin/stitch-wiki.js
+++ bin/stitch-wiki.js
@@ -1,20 +1,26 @@
 #!/usr/bin/env node
 
 /*
-Stitch a TWPub file into a TW5 HTML file
+Stitch multiple TWPub or other JSON files into a TW5 HTML file
 
-node ./bin/stitch-wiki.js <path-to-twpub-file> <path-to-input-wiki> <path-to-output-wiki>
+node ./bin/stitch-wiki.js <path-to-input-wiki> <path-to-output-wiki> <path-to-twpub-file> <path-to-twpub-file> ...
+
+e.g.:
+
+node ./bin/stitch-wiki.js ./tmp/empty.html ./tmp/index.html mytwpub.json anothertiddler.json
 */
 
 const fs = require("fs");
 
 // Get arguments
-const pathTWPub = process.argv[2],
-	pathInputWiki = process.argv[3],
-	pathOutputWiki = process.argv[4];
+const pathInputWiki = process.argv[2],
+	pathOutputWiki = process.argv[3],
+	arrayPathJson = process.argv.slice(4);
+
+	console.log(pathInputWiki,pathOutputWiki,arrayPathJson)
 
 // Check arguments are not missing/empty
-if(!pathTWPub || !pathInputWiki || !pathOutputWiki) {
+if(!arrayPathJson || !arrayPathJson[0] || !pathInputWiki || !pathOutputWiki) {
 	throw "Missing arguments";
 }
 
@@ -24,11 +30,20 @@ const marker = "<script class=\"tiddlywiki-tiddler-store\" type=\"application/js
 // Get the existing wiki file and split it by the marker
 const templateParts = fs.readFileSync(pathInputWiki,"utf8").split(marker);
 
-// Get the text of the TWPub file
-const textTWPub = fs.readFileSync(pathTWPub,"utf8");
-
-// Assemble the payload 
-const payload = textTWPub + ",\n";
+// Get the text of the JSON files and assemble the payload
+var payload = "";
+for(const pathJson of arrayPathJson) {
+	const text = fs.readFileSync(pathJson,"utf8"),
+		json = JSON.parse(text);
+	if(Array.isArray(json)) {
+		// If it is an array of tiddlers, flatten them down to individual tiddlers
+		for(const item of json) {
+			payload += JSON.stringify(item) + ",\n";
+		}
+	} else {
+		payload += text + ",\n";
+	}
+}
 
 // Assemble the new wiki file
 const html = templateParts[0] + marker + payload + templateParts[1];
